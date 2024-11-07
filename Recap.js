29dd33e5b70f4ae97e9493eccc66f352afafe775
KINGALVI <alvialmahamud@gmail.com>
2024-11-07 23:49:53 +0200

    add some code

---
+++ Recap.js
@@ -0,0 +1,11 @@
+// Example 1
+
+let alvi = 5;
+
+let alviFuction = ( num1 , num2 ) =>{
+    let result = num1 + num2 + alvi;
+    return result;
+}
+
+const alviResult = alviFuction( 5 , 5 );
+console.log(alviResult)
